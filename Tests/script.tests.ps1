3e9bbeea3c81b8e6b7fe1f8747298409ff919081
fatimanisha <121461061+fatimanisha@users.noreply.github.com>
2024-10-20 23:13:31 -0400

    Update script.tests.ps1

--- Tests/script.tests.ps1
+++ Tests/script.tests.ps1
@@ -7,12 +7,12 @@ Describe 'PowerShell Script Tests' {
     Mock New-AzVM { "Mocked Virtual Machine" }
 
     It 'Should create a resource group' {
-        . ./YourScript.ps1  # Import the PowerShell script
+        . ./CreateVM.ps1  # Import the PowerShell script
         Assert-MockCalled -CommandName New-AzResourceGroup -Exactly -Times 1
     }
 
     It 'Should create a virtual machine' {
-        . ./YourScript.ps1  # Import the PowerShell script
+        . ./CreateVM.ps1  # Import the PowerShell script
         Assert-MockCalled -CommandName New-AzVM -Exactly -Times 1
     }
 }
